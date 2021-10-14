fd2f5410512b9132be4fc498d59f6afdd7c06ad0
s18ud1120044 <s18ud1120044@nmit.edu.mn>
2021-10-14 15:07:24 +0800

    Login redo

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -31,45 +31,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Stack(
           children: <Widget>[
-            Image.asset('assets/images/bg.png'), // Background
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                OutlinedButton( // Button to switch to Screen 2
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute (
-                        builder: (BuildContext context) => const Screen2(title: 'Screen 2'),
-                      ),
-                    );
-                  }, child:
-                  const Text(
-                    'Screen 2'
+            Image.asset('assets/images/bg.png'),
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(
+                    // Space for formatting
+                    height: 300,
                   ),
-                ),
-                OutlinedButton( // Button to switch to Screen 3
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute (
-                        builder: (BuildContext context) => const Screen3(title: 'Screen 3'),
-                      ),
-                    );
-                  },
-                  child:
                   const Text(
-                      'Screen 3'
+                    'Log-in',
                   ),
-                ),
-              ],
+                  const Text(
+                    'E-mail',
+                  ),
+                  const TextField(
+                    // Input field for name
+                    decoration: InputDecoration(
+                      hintText: 'Enter your e-mail address',
+                    ),
+                  ),
+                  const Text(
+                    'Email',
+                  ),
+                  const Text(
+                    'Password',
+                  ),
+                  const TextField(
+                    // Input field for password
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  Row(
+                    // Row for buttons
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const MyHomePage(title: 'Screen 3'),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Forget password?',
+                        ),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Log-in',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Dont't have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const MyHomePage(title: 'Screen2'),
+                            ),
+                          );
+                        },
+                        child: Text(''),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
