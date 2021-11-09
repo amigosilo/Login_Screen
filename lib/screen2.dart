0798fd39808300912188688682079c0176b03fee
Name of your next sex tape <s18ud1120044@nmit.edu.mn>
2021-11-09 09:06:23 +0800

    +

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:login_screen/screen1.dart';
 
@@ -11,7 +13,6 @@ class Screen2 extends StatefulWidget {
 }
 
 class _Screen2State extends State<Screen2> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,30 +32,28 @@ class _Screen2State extends State<Screen2> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    const SizedBox( // Space for formatting
-                      height: 300 ,
+                    const SizedBox(
+                      // Space for formatting
+                      height: 300,
                     ),
                     const Text(
                       'Sign-up',
                       style: TextStyle(
-                          fontSize:40.0,
+                          fontSize: 40.0,
                           fontWeight: FontWeight.bold,
-                          fontFamily: 'Times New Roman'
-                      ),
+                          fontFamily: 'Times New Roman'),
                     ),
-                    const SizedBox(
-                        width: 20,
-                        height:20
-                    ),
+                    Container(width: 20, height: 20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         CircleAvatar(
                           radius: 50,
                           backgroundColor: Colors.black12,
-                          child: IconButton( // Button for adding profile picture (function not implemented)
+                          child: IconButton(
+                            // Button for adding profile picture (function not implemented)
                             color: Colors.grey,
-                            onPressed: (){}, // Placeholder function
+                            onPressed: () {}, // Placeholder function
 
                             icon: const Icon(
                               Icons.camera_alt,
@@ -64,109 +63,85 @@ class _Screen2State extends State<Screen2> {
                         ),
                       ],
                     ),
-                    const SizedBox(
-                        width: 20,
-                        height:20
-                    ),
-                    const Text(
-                      'Name',
+                    Container(width: 20, height: 20),
+                    const Text('Name',
                         style: TextStyle(
-                          fontSize:20.0,
+                          fontSize: 20.0,
                           fontWeight: FontWeight.bold,
-                        )
-                    ),
-
-                    const TextField( // Input field for name
+                        )),
+                    const TextField(
+                      // Input field for name
                       decoration: InputDecoration(
                         hintText: 'Your Name',
                       ),
                     ),
-                    const Text(
-                      'Email',
+                    const Text('Email',
                         style: TextStyle(
-                          fontSize:20.0,
+                          fontSize: 20.0,
                           fontWeight: FontWeight.bold,
-                        )
-                    ),
-                    const TextField( // Input field for email
+                        )),
+                    const TextField(
+                      // Input field for email
                       decoration: InputDecoration(
                         hintText: 'Your Email',
                       ),
                     ),
-                    const Text(
-                      'Contact No.',
+                    const Text('Contact No.',
                         style: TextStyle(
-                          fontSize:20.0,
+                          fontSize: 20.0,
                           fontWeight: FontWeight.bold,
-                        )
-                    ),
-                    const TextField( // Input field for contact number
+                        )),
+                    const TextField(
+                      // Input field for contact number
                       decoration: InputDecoration(
                         hintText: 'Your Contact Number',
                       ),
                     ),
-                    const Text(
-                      'Password',
+                    const Text('Password',
                         style: TextStyle(
-                          fontSize:20.0,
+                          fontSize: 20.0,
                           fontWeight: FontWeight.bold,
-                        )
-                    ),
-                    const TextField( // Input field for password
+                        )),
+                    const TextField(
+                      // Input field for password
                       decoration: InputDecoration(
                         hintText: 'Password',
                       ),
                       obscureText: true,
                     ),
-                    const SizedBox(
-                        width: 20,
-                        height:20
-                    ),
-
+                    Container(width: 20, height: 20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SizedBox(
+                        Container(
                           height: 50.0,
                           width: 300,
-                          child: RaisedButton( // Button to sign-up (function not implemented)
-                            onPressed: (){}, // Placeholder function
+                          child: RaisedButton(
+                              // Button to sign-up (function not implemented)
+                              onPressed: () {}, // Placeholder function
 
-                            shape: RoundedRectangleBorder(
-
-                                borderRadius: BorderRadius.circular(80.0)
-                            ),
-                            child: const Text(
-                              'Sign-up',
-                              style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.white
-                              )
-
-                            ),
-                              color: Colors.black
-
-
-                          ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0)),
+                              child: const Text('Sign-up',
+                                  style: TextStyle(
+                                      fontSize: 25, color: Colors.white)),
+                              color: Colors.black),
                         ),
                       ],
                     ),
-                    TextButton( // Button to cancel sign-up and return to Screen 1
-                      onPressed: (){
+                    TextButton(
+                      // Button to cancel sign-up and return to Screen 1
+                      onPressed: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (BuildContext context) => const MyHomePage(title: 'Screen 1'),
+                            builder: (BuildContext context) =>
+                                const MyHomePage(title: 'Screen 1'),
                           ),
                         );
                       },
-                      child: const Text(
-                        'Back to Login',
-                          style: TextStyle(
-                            color: Colors.black
-
-                          )
-                      ),
+                      child: const Text('Back to Login',
+                          style: TextStyle(color: Colors.black)),
                     ),
                   ],
                 ),
